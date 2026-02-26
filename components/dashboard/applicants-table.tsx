9ca8b389345f048b995473e881d8f56353ccda2e
Jaco Kotzee <jakwakwa@gmail.com>
2026-02-27 00:34:18 +0200

    Refactor: Organize imports in applicants table

--- components/dashboard/applicants-table.tsx
+++ components/dashboard/applicants-table.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { RiArrowDownSLine, RiArrowUpSLine, RiMoreLine } from "@remixicon/react";
+import type { ColumnDef } from "@tanstack/react-table";
 import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { DataTable } from "@/components/ui/data-table";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -12,9 +15,6 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
-import { RiArrowDownSLine, RiArrowUpSLine, RiMoreLine } from "@remixicon/react";
-import type { ColumnDef } from "@tanstack/react-table";
-import { DataTable } from "@/components/ui/data-table";
 
 // --- Types ---
 
